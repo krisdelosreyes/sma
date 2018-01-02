a7047074b0343f463c52968b9e473a98feba692d
Kristoffer delos Reyes <kristoffer@embassydigital.com.au>
2018-01-02 10:53:18 +0800

    modified css and js for spinraza

--- spinraza/js/spinraza-base.js
+++ spinraza/js/spinraza-base.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
+
+    $('#auth').on('change', function() {
+          if ( this.value == '1')
+          {
+            $(".h-pro").show();
+            $("#user-login").attr("action", "/content/commercial-au/specialty/spinraza/hcp/en_au/home/hcp.html");
+          }
+
+          else
+          {
+            $(".h-pro").hide();
+          }
+
+          if ( this.value == '2')
+          {
+            $(".h-patient").show();
+            $("#user-login").attr("action", "/content/commercial-au/specialty/spinraza/hcp/en_au/home/patient-resources.html");
+          }
+
+          else
+          {
+            $(".h-patient").hide();
+          }
+    });
+
     if (!$("body").hasClass(".page_hcp-home")) {
-    	$('.hcp-text p').show();
+        $('.hcp-text p').show();
     }
-    if (!$("body").hasClass(".page_home")) {
-    	$('.header.spinraza-header, .main-nav-list').hide();
+    else {
+        $('.hcp-text p').hide();
     }
 });
 
@@ -13,14 +38,14 @@ $("#request-pword").click(function(){
 
 
 $('.cg-video').parent().click(function () {
-	if(
-		$(this).children(".cg-video").get(0).paused){
-		$(this).children(".cg-video").get(0).play();
-		$(this).children(".cg-playpause").fadeOut(); }
+    if(
+        $(this).children(".cg-video").get(0).paused){
+        $(this).children(".cg-video").get(0).play();
+        $(this).children(".cg-playpause").fadeOut(); }
 
-	else {
+    else {
 
-		$(this).children(".cg-video").get(0).pause();
-		$(this).children(".cg-playpause").fadeIn();
-	}
+        $(this).children(".cg-video").get(0).pause();
+        $(this).children(".cg-playpause").fadeIn();
+    }
 });
