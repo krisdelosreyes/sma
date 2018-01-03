4d00e020e1ab3657a2625777f78bd1092b8b1c73
xdisclaimerx <xtian.amaro@gmail.com>
2018-01-03 08:39:04 +0800

    fixed issues from feedback

--- js/hcp/sma-hcp-modal.js
+++ js/hcp/sma-hcp-modal.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var buttongroup = $('<div class="checkbox"><input class="hcp-checkbox" type="checkbox"><b>Yes,</b> I confirm that I am a healthcare professional registered in Australia<br></div><div class="proceed-button"><a href="https://contentprod-spinraza-com-au.biogen-support.com" target="_blank" id="continue-button3" class="proceed-link"><img class="proceed" src="/content/dam/commercial-au/specialty/sma/hcp/en_au/images/icons/button_proceed.png"/></a></div><a href="#" class="cancel-link" data-dismiss="modal">Cancel</a>');
+  var buttongroup = $('<div class="checkbox"><input class="hcp-checkbox" type="checkbox"><span class="hcp-checkmark"></span><b>Yes,</b> I confirm that I am a healthcare professional registered in Australia<br></div><div class="proceed-button"><a href="https://contentprod-spinraza-com-au.biogen-support.com" target="_blank" id="continue-button3" class="proceed-link">Proceed</a></div><a href="#" class="cancel-link" data-dismiss="modal">Cancel</a>');
 
   $(".exit-site .modal-buttons").append(buttongroup);
   $(".third-exit-site .modal-buttons").append(buttongroup);
