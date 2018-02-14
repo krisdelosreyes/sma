580f343609a3b15508a0b347f02152bdef0c8dac
xdisclaimerx <xtian.amaro@gmail.com>
2018-02-14 18:00:50 +0800

    add script for spinraza typekit implementation

--- js/caregiver/sma-caregiver-modal.js
+++ js/caregiver/sma-caregiver-modal.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   // var buttongroup = $('<div class="checkbox"><input class="hcp-checkbox" type="checkbox"><b>Yes,</b> I confirm that I am a healthcare professional registered in Australia<br></div><div class="proceed-button"><a target="_blank" href="https://www.togetherinsma-hcp.com.au" id="continue-button3" class="proceed-link"><img class="proceed" src="/content/dam/commercial-au/specialty/sma/caregiver/en_au/images/icons/button_proceed.png"/></a></div><a href="#" class="cancel-link" data-dismiss="modal">Cancel</a>');
 
   //content-prod
-  var buttongroup = $('<div class="checkboxFive"><input type="checkbox" id="checkboxFiveInput" name=""/><label for="checkboxFiveInput"></label><b>Yes,</b> I confirm that I am a healthcare professional registered in Australia<br></div><div class="proceed-button"><a href="https://contentprod-togetherinsmahcp-com-au.biogen-support.com"  id="continue-button3" class="proceed-link">Proceed</a></div><a href="#" class="cancel-link" data-dismiss="modal">Cancel</a>');
+  var buttongroup = $('<div class="checkboxFive"><input type="checkbox" id="checkboxFiveInput" name=""/><label for="checkboxFiveInput"></label><b>Yes,</b> I confirm that I am a healthcare professional registered in Australia<br></div><div class="proceed-button not-active"><a href="https://contentprod-togetherinsmahcp-com-au.biogen-support.com"  id="continue-button3" class="proceed-link">Proceed</a></div><a href="#" class="cancel-link" data-dismiss="modal">Cancel</a>');
 
 
 
